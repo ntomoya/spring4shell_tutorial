a9d9720002a876f3dafdcadd38f3528878d720ee
Tomoya Nakanishi <tomoya_nakanishi@r.recruit.co.jp>
2022-07-22 14:01:04 +0900

    WorldModelを追加

--- src/main/java/com/example/spring4shell/HelloModel.java
+++ src/main/java/com/example/spring4shell/HelloModel.java
@@ -2,6 +2,7 @@ package com.example.spring4shell;
 
 public class HelloModel {
     public String hello;
+    public WorldModel world = new WorldModel();
 
     public String getHello() {
         return hello;
@@ -9,5 +10,9 @@ public class HelloModel {
 
     public void setHello(String hello) {
         this.hello = hello;
+    }
+
+    public WorldModel getWorld() {
+        return world;
     }
 }
---
+++ src/main/java/com/example/spring4shell/WorldModel.java
@@ -0,0 +1,13 @@
+package com.example.spring4shell;
+
+public class WorldModel {
+    public String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
