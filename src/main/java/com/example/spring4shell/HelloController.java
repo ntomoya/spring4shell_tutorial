007430244cbf4d39d7807db63a66a2fdcf0fa5d7
Tomoya Nakanishi <tomoya_nakanishi@r.recruit.co.jp>
2022-07-26 10:00:50 +0900

    エンドポイント（/world）を追加

--- src/main/java/com/example/spring4shell/HelloController.java
+++ src/main/java/com/example/spring4shell/HelloController.java
@@ -1,9 +1,5 @@
 package com.example.spring4shell;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +9,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(HelloModel hello) {
         return hello.hello;
+    }
+
+    @RequestMapping("/world")
+    public String world(HelloModel hello) {
+        return hello.world.message;
     }
 }
